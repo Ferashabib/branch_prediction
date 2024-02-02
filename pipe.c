693825fadfb8f2822967b3664469726e44bfd52b
Ferashabib <124314075+Ferashabib@users.noreply.github.com>
2024-02-02 14:49:29 -0800

    execlp 2

--- pipe.c
+++ pipe.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
+//#include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
 	printf("I am lazy, letting other program to ls the directory\n\n");
 	printf("I will just wait for their report\n\n");
-	execlp(argc[1], argv[1], argv[2][1], argv[2][2], NULL);
+	execlp("ls", "ls", NULL);
 	printf ("They finished; Done! \n");
 	return 0;
 }
