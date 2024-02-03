b9cde975b934e11ec8b4f988323c1bee0d8282bf
Ferashabib <124314075+Ferashabib@users.noreply.github.com>
2024-02-02 19:34:52 -0800

    final edits in pipe.c

--- pipe.c
+++ pipe.c
@@ -8,34 +8,39 @@
 int main(int argc, char *argv[])
 {
 	if (argc <= 1) { //Check if there are any arguments
-		printf("No Arguments");
+		//printf("No Arguments");
 		exit(EINVAL);
 	}
 
-	for (int i = 1; i < argc; i++) {
+	for (int i = 1; i < argc; i++) { //argv[0] is the program itself so we start at 1
 		int fds[2];
 		pipe(fds); //create the pipe
 
 		int ret = fork();
 		if (ret == 0) {//child
 			if (i != argc-1) {//Not the last argument
+
+				// dup2() performs the same task as dup(), but it specifically redirects fd.
+				//output will go to write end of fds[1] instead of stdout
 				dup2(fds[1], 1);
 			}
 
 			close(fds[0]);
 			close(fds[1]);
 		      
+			//int execlp(const char *file, const char *arg, ... /*(char*) NULL*/);  
 			execlp(argv[i], argv[i], NULL);
 
-			printf("Execlp failed!");
+			//printf("Execlp failed!");
 			return (EXIT_FAILURE); //if execlp fails
 		}
 		else if (ret>0) { //parent
-			dup2(fds[0], 0);
+			dup2(fds[0], 0); //redirect stdin to pipe
 
 			close(fds[0]);
 			close(fds[1]);
 
+			//pid_t waitpid(pid_t pid, int *wstatus, int options);
 			int wstatus;
 			waitpid(ret, &wstatus, 0);
 
@@ -46,13 +51,15 @@ int main(int argc, char *argv[])
 			int code = WEXITSTATUS(wstatus);
 
 			if (child_st && code) {
-				printf("Oop!");
+
+				//printf("Oop!");
 				exit(code);
 			}
 
 		}
 		else { //error
-			printf("No fork, just spoons");
+
+			//printf("No fork, just spoons");
 			exit(EXIT_FAILURE);
 		}
 	}
