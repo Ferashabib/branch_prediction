c231fce92371ff3acaeb7dc695e795effa374a33
Ferashabib <124314075+Ferashabib@users.noreply.github.com>
2024-04-21 22:16:51 -0700

    fsm changes

--- cbp2-infrastructure-v2/src/my_predictor.h
+++ cbp2-infrastructure-v2/src/my_predictor.h
@@ -11,8 +11,8 @@ public:
 
 class my_predictor : public branch_predictor {
 public:
-#define HISTORY_LENGTH	15
-#define TABLE_BITS	15
+#define HISTORY_LENGTH	18
+#define TABLE_BITS	18
 	my_update u;
 	branch_info bi;
 	unsigned int history;
