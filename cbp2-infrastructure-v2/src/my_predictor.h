3ddf891151adfe683a7f88234dfbc87534b1db1c
Ferashabib <124314075+Ferashabib@users.noreply.github.com>
2024-04-21 23:40:08 -0700

    g skew method

--- cbp2-infrastructure-v2/src/my_predictor.h
+++ cbp2-infrastructure-v2/src/my_predictor.h
@@ -13,8 +13,8 @@ public:
 
 class my_predictor : public branch_predictor {
 public:
-#define HISTORY_LENGTH	17
-#define TABLE_BITS	30
+#define HISTORY_LENGTH	18
+#define TABLE_BITS	18
 	my_update u;
 	branch_info bi;
 	unsigned int history;
