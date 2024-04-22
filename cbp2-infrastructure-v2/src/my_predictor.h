93353dbda230c4f2c1fe61a4a0f47e2492960149
Ferashabib <124314075+Ferashabib@users.noreply.github.com>
2024-04-21 23:37:04 -0700

    g skew method

--- cbp2-infrastructure-v2/src/my_predictor.h
+++ cbp2-infrastructure-v2/src/my_predictor.h
@@ -6,29 +6,42 @@
 
 class my_update : public branch_update {
 public:
-	unsigned int index;
+	unsigned int index1;
+	unsigned int index2;
+	unsigned int index3;
 };
 
 class my_predictor : public branch_predictor {
 public:
-#define HISTORY_LENGTH	17
-#define TABLE_BITS	30
+#define HISTORY_LENGTH	15
+#define TABLE_BITS	15
 	my_update u;
 	branch_info bi;
 	unsigned int history;
-	unsigned char tab[1<<TABLE_BITS];
+	unsigned char tab1[1<<TABLE_BITS];
+	unsigned char tab2[1<<TABLE_BITS];
+	unsigned char tab3[1<<TABLE_BITS];
 
 	my_predictor (void) : history(0) { 
-		memset (tab, 0, sizeof (tab));
+		memset (tab1, 0, sizeof (tab1));
+		memset (tab2, 0, sizeof (tab2));
+		memset (tab3, 0, sizeof (tab3));
 	}
 
 	branch_update *predict (branch_info & b) {
 		bi = b;
 		if (b.br_flags & BR_CONDITIONAL) {
-			u.index = 
+			u.index1 = 
 				  (history << (TABLE_BITS - HISTORY_LENGTH)) 
-				^ (b.address & ((1<<TABLE_BITS)-1));
-			u.direction_prediction (tab[u.index] >> 1);
+				^ (b.address & ((1<<10)-1));
+			u.index2 = 
+				  (history << (TABLE_BITS - HISTORY_LENGTH)) 
+				^ ((b.address>>10) & ((1<<10)-1));
+			u.index3 = 
+				  (history << (TABLE_BITS - HISTORY_LENGTH)) 
+				^ ((b.address>>20) & ((1<<10)-1));
+			int FinalIndex = ((tab1[u.index1] >> 1) + (tab2[u.index2] >> 1) +(tab3[u.index3] >> 1));
+			u.direction_prediction ((FinalIndex > 1) ? 1 : 0);
 		} else {
 			u.direction_prediction (true);
 		}
@@ -38,11 +51,17 @@ public:
 
 	void update (branch_update *u, bool taken, unsigned int target) {
 		if (bi.br_flags & BR_CONDITIONAL) {
-			unsigned char *c = &tab[((my_update*)u)->index];
+			unsigned char *c1 = &tab1[((my_update*)u)->index1];
+			unsigned char *c2 = &tab2[((my_update*)u)->index2];
+			unsigned char *c3 = &tab3[((my_update*)u)->index3];
 			if (taken) {
-				if (*c < 3) (*c)++;
+				if (*c1 < 3) (*c1)++;
+				if (*c2 < 3) (*c2)++;
+				if (*c3 < 3) (*c3)++;
 			} else {
-				if (*c > 0) (*c)--;
+				if (*c1 > 0) (*c1)--;
+				if (*c2 > 0) (*c2)--;
+				if (*c3 > 0) (*c3)--;
 			}
 			history <<= 1;
 			history |= taken;
